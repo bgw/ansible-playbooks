a8d0356688745be432eb7d8788645579bd5dbcb5
Benjamin Woodruff <benjaminwoodruff@ufl.edu>
2014-08-26 23:28:38 -0400

    Update to ansible 1.7
    - Temporary fix for ansible's new variable injection protection
    - Use cpanm module for ddclient

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ from setuptools import find_packages
 setup(name="pipeep-ansible-playbooks",
       package_dir={"": "plugins"},
       packages=["jinja2_plugins"],
-      install_requires=["ansible>=1.5,<1.6"])
+      install_requires=["ansible>=1.7,<1.8"])
